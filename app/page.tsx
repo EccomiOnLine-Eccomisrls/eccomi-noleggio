745805d403ea104c43b85ba10683a4cb6a0ae913
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-07-28 22:35:42 +0200

    Attiva i comandi gestione promozioni

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import DashboardClient from "./dashboard-client";
+import PromotionManagementControls from "./promotion-management-controls";
 
 export default function Home() {
-  return <DashboardClient />;
+  return (
+    <>
+      <DashboardClient />
+      <PromotionManagementControls />
+    </>
+  );
 }
