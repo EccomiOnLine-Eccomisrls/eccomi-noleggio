36c314487a4e26352216db6f45a76270e856f561
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-07-30 17:40:08 +0200

    Rimuove il bridge dalla pagina richiesta

--- app/richiesta/page.tsx
+++ app/richiesta/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./request-upload.css";
 import RequestClient from "./request-client";
-import SequentialUploadBridge from "./sequential-upload-bridge";
 
 export const metadata: Metadata = {
   title: "Richiesta di noleggio | ECCOMI NOLEGGIO",
@@ -16,5 +15,5 @@ export default async function RequestPage({
 }) {
   const params = await searchParams;
   const promotionId = typeof params.promozione === "string" ? params.promozione : "";
-  return <><SequentialUploadBridge /><RequestClient promotionId={promotionId} /></>;
+  return <RequestClient promotionId={promotionId} />;
 }
