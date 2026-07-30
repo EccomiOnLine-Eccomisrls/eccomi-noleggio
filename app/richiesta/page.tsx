b32d042e96129ef98b8af0b0b1010d70b5f7cdfd
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-07-30 08:36:15 +0200

    Attiva invio sequenziale documenti nel wizard

--- app/richiesta/page.tsx
+++ app/richiesta/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./request-upload.css";
 import RequestClient from "./request-client";
+import SequentialUploadBridge from "./sequential-upload-bridge";
 
 export const metadata: Metadata = {
   title: "Richiesta di noleggio | ECCOMI NOLEGGIO",
@@ -15,5 +16,5 @@ export default async function RequestPage({
 }) {
   const params = await searchParams;
   const promotionId = typeof params.promozione === "string" ? params.promozione : "";
-  return <RequestClient promotionId={promotionId} />;
+  return <><SequentialUploadBridge /><RequestClient promotionId={promotionId} /></>;
 }
