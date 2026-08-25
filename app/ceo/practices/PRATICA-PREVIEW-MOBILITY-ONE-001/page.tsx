a896321a454cfd808463372544cf5d7790eeb708
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-25 18:50:10 +0200

    Fix preview route pratica Mobility One

---
+++ app/ceo/practices/PRATICA-PREVIEW-MOBILITY-ONE-001/page.tsx
@@ -0,0 +1,7 @@
+import CeoPracticePage from "../[id]/page";
+
+export default function PreviewMobilityOnePracticePage() {
+  return CeoPracticePage({
+    params: Promise.resolve({ id: "PRATICA-PREVIEW-MOBILITY-ONE-001" }),
+  });
+}
