4cb54a358aedebabd49bb253c8ce2dce920bed95
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-25 16:31:49 +0200

    fix(preview): route Ypsilon demo offer

---
+++ app/ceo/promotions/preview-promo-2/page.tsx
@@ -0,0 +1,6 @@
+import { previewPromotionEditableYpsilon } from "../../../lib/server/preview-fixture";
+import PreviewOfferView from "../preview-offer-view";
+
+export default function PreviewPartnerOfferTwoPage() {
+  return <PreviewOfferView promotion={previewPromotionEditableYpsilon} />;
+}
