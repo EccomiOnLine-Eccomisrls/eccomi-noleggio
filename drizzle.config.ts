b5829b6854c47ee236529407d28bdd81976dbd4f
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-08-26 03:59:15 +0200

    PR13: include schema permessi Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./db/schema.ts",
+  schema: ["./db/schema.ts", "./db/access-schema.ts"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
